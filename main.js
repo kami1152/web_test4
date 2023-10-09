9cffef0f7ee56d201025cb0e52fe7da34d69b4e0
starsong <kami1152@naver.com>
2023-10-09 16:59:13 +0900

    1009:1600

--- main.js
+++ main.js
@@ -1,83 +1,94 @@
-let computerNumber = 0;
-let playButton = document.getElementById("play-button");
-let resetButton = document.querySelector(".button-reset");
-let userInput = document.querySelector("#user-input");
-let resultAreaImg = document.querySelector(".main-img");
-let resultText = document.querySelector(".result-text");
-let chanceArea = document.getElementById("chance-area");
-let gameOver = false;
-let chances = 5; // 남은 기회
-let userValueList = []; // 유저가 입력한 숫자들 리스트
+// 랜덤 번호 지정
+// 유저가  번호를 입력한다. 그리고 .go라는 버튼을 누름
+// 만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
+// 랜덤번호가 < 유저번호 Down
+// 랜덤번호가 > 유저번호 up
+// reset버튼을 누르면 게임이 리셋된다.
+// 5번의 기회를 다쓰면 게임이 끝난다. (더이상 추측 불가, 버튼이 disable)
+// 유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깎지 않는다.
+// 유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깎지 않는다.
 
-chanceArea.innerHTML = `남은 기회:${chances}`;
-playButton.addEventListener("click", play);
-resetButton.addEventListener("click", reset);
-userInput.addEventListener("focus", focusInput);
+let randomNumber = 0;
+let inputNum = document.getElementById("inputNumber");
+let startBtn = document.getElementById("startButton");
+let result = document.getElementById("result");
+let resetBtn = document.getElementById("resetButton");
+let chanceArea = document.getElementById("chance");
+let chance = 5;
+let gameEnd = false;
+let inputNumList = [];
+let change = document.getElementById("changeImg");
 
-function pickRandomNumber() {
-  // 랜덤숫자 뽑기
+startBtn.style.cursor="pointer";
 
-  computerNumber = Math.floor(Math.random() * 100) + 1;
-  console.log("정답", computerNumber);
+// 랜덤 번호 지정.
+function computerNum(){
+    randomNumber = Math.floor(Math.random()*100)+1;
+    console.log(randomNumber);
 }
+// 버튼
+startBtn.addEventListener("click", start);
+resetBtn.addEventListener("click", reset);
 
-function play() {
-  // 숫자 추측하기
-  const userValue = userInput.value;
-  if (userValue < 1 || userValue > 100) {
-    resultText.textContent = "1부터 100 사이의 숫자를 입력 해주세요";
+// 마우스로 입력창을 클릭할때 입력되어 있는 값을 리셋
+inputNum.addEventListener("focus", function(){
+    inputNum.value="";
+})
 
-    return;
-  }
+// startbutton을 눌렀을 때 반응
+function start(){
+    let inputNumValue = inputNum.value;
 
-  if (userValueList.includes(userValue)) {
-    resultText.textContent = "이미 입력한 숫자입니다. 다른 숫자를 입력해주세요";
+    if(inputNumValue > 100 || inputNumValue < 1){
+        result.textContent = "1부터 100사이의 숫자만 입력해주세요."
+        return;
+    }
 
-    return;
-  }
+    if(inputNumList.includes(inputNumValue)){
+        result.textContent = "이미 입력한 숫자입니다."
+        return;
+    }
 
-  chances--;
-  chanceArea.innerHTML = `남은 기회:${chances}`;
-  userValueList.push(userValue);
-  if (userValue < computerNumber) {
-    resultAreaImg.src =
-      "https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif";
-    resultText.textContent = "Up!";
-  } else if (userValue > computerNumber) {
-    resultAreaImg.src = "https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif";
-    resultText.textContent = "Down!";
-  } else {
-    resultAreaImg.src =
-      "https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif";
-    resultText.textContent = "정답!";
-    gameOver = true;
-  }
+    chance--;
 
-  if (chances == 0) {
-    gameOver = true;
-  }
+    chanceArea.textContent =`남은 기회 : ${chance}`;
 
-  if (gameOver == true) {
-    playButton.disabled = true;
-  }
-}
+    if(inputNumValue < randomNumber){
+        change.src="Images/up.gif"
+        result.textContent = "UP!!";
+        
+    } else if(inputNumValue > randomNumber){
+        result.textContent = "DOWN!!";
+        change.src="Images/down.gif"
+    } else {
+        result.textContent = "정답입니다!!!";
+        change.src="Images/ok.gif"
+        gameEnd=true;
+    }
 
-function focusInput() {
-  userInput.value = "";
-}
+    inputNumList.push(inputNumValue);
+    
+    if(chance==0){
+        gameEnd = true;
+    }
 
-function reset() {
-  //리셋
-  pickRandomNumber();
-  userInput.value = "";
-  resultAreaImg.src =
-    "https://media1.giphy.com/media/9DinPR8bzFsmf74j9W/giphy.gif";
-  resultText.textContent = "죽기 싫다면 맞춰라";
-  gameOver = false;
-  playButton.disabled = false;
-  chances = 5;
-  chanceArea.innerHTML = `남은 기회:${chances}`;
-  userValueList = [];
-}
+    if(gameEnd==true){
+        startBtn.disabled= true;
+        startBtn.style.backgroundColor="rgba(75, 87, 255, 0.4)";
+        startBtn.style.cursor = "default";
+    }
+};
 
-pickRandomNumber();
+// 리셋버튼 함수
+function reset(){
+    inputNumValue="";
+    computerNum();
+    gameEnd = false;
+    chance = 5;
+    startBtn.disabled =false;
+    chanceArea.textContent =`남은 기회 : ${chance}`;
+    inputNumList = [];
+    change.src="Images/go!.gif"
+};
+// 함수를 실행하는 것.
+computerNum();
